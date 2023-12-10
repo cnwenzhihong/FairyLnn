37efdefe8239c3c4f965be2d9040effbe96ba6c8
wzh <2423113760@qq.com>
2023-12-10 17:16:36 +0800

    测试

---
+++ Source/ImmortalLnn/ImmortalLnn.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ImmortalLnn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ImmortalLnn, "ImmortalLnn" );
